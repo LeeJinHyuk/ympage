d6c90d620be0ef929d87119a91114cf1a4aaa1bd
eerto <eerto@naver.com>
2017-05-25 00:33:39 +0900

    install modules and insert test code

--- source/app/app.js
+++ source/app/app.js
@@ -1,1 +1,4 @@
-console.log("test");
+import {Render} from "react-dom";
+// import {Test} from "./test";
+
+Render(<div />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,35 @@
-import webpack from "webpack";
-import path from "path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import WebpackCleanupPlugin from "webpack-cleanup-plugin";
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackCleanupPlugin = require("webpack-cleanup-plugin");
 
 module.exports = {
   entry : {
-    app : "./source/app/app.js"
+    app : "./source/app/app.js",
+    vendor: ["react", "react-dom"]
   },
   output : {
     path : path.join(__dirname, "source/dist"),
     filename : "[name]-[hash].js",
     publicPath : "source/app"
+  },
+  module : {
+    rules :
+    [
+      {
+        test : /\.jsx?$/,
+        loader : "babel-loader",
+        options :
+        {
+          presets :
+            [["es2015",{module:"false"}], "react"],
+        },
+        exclude :
+        [
+          "node_modules"
+        ],
+      }
+    ],
   },
   plugins : [
     new HtmlWebpackPlugin({
