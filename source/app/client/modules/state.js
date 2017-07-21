bf67f6d1b46989d9f8ffdebee55644413fa6181e
LeeJinHyuk <eerto@naver.com>
2017-07-21 09:53:24 +0900

    add test code about redux

---
+++ source/app/client/modules/state.js
@@ -0,0 +1,18 @@
+// Actions
+const CHANGE_TAB = "app/modules/state/CHANGE_TAB";
+
+// Init value
+const init_value = {
+    index : "0"
+};
+
+// Action Creators
+export const changeTab = (_index) => ({
+    type : CHANGE_TAB,
+    index : _index
+});
+
+// Reducer
+export default reducer = (_state, _action) => ({
+
+});
