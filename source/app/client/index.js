423d27f8df93ab54fc15e317eb57ed5d0ed68a74
LeeJinHyuk <eerto@naver.com>
2017-07-24 01:07:55 +0900

    update modules loading path

--- source/app/client/index.js
+++ source/app/client/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDom from "react-dom";
 import {createStore} from "redux";
 import {Provider} from "react-redux";
-import Reducer from "modules/index";
+import Reducer from "modules/state";
 import Root from "root";
 
 let store = createStore(Reducer);
--- source/app/client/modules/state.js
+++ source/app/client/modules/state.js
@@ -6,20 +6,20 @@ const init_value = {
     tab_index : "0"
 };
 
-// Action Creators
-export const changeTab = (_index) => ({
-    type : CHANGE_TAB,
-    tab_index : _index
-});
-
 // Reducer
-export default reducer = (_state = init_value, _action) => {
+export default function reducer (_state = init_value, _action = {}) {
     switch(_action.type) {
         case CHANGE_TAB :
             return Object.assign({}, _state, {
                 tab_index : _state.tab_index
             });
         default :
-            return state;
+            return _state;
     }
 };
+
+// Action Creators
+export const changeTab = (_index) => ({
+    type : CHANGE_TAB,
+    tab_index : _index
+});
