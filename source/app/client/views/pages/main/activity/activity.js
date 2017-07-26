09c9c60ef9b106362db4591aeb1b50714e75e47f
lee jin hyuk <leejh@comica.com>
2017-07-26 18:54:19 +0900

    add lifecycle test code

--- source/app/client/views/pages/main/activity/activity.js
+++ source/app/client/views/pages/main/activity/activity.js
@@ -2,6 +2,41 @@ import React, {Component} from "react";
 
 class Activity extends Component {
 
+    constructor(props) {
+        console.log("constructor");
+        super(props);
+    }
+
+    componentWillMount() {
+        console.log("componentWillMount");
+    }
+
+
+    componentDidMount() {
+        console.log("componentDidMount");
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log("componentWillReceiveProps: " + JSON.stringify(nextProps));
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log("shouldComponentUpdate: " + JSON.stringify(nextProps) + " " + JSON.stringify(nextState));
+        return true;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log("componentWillUpdate: " + JSON.stringify(nextProps) + " " + JSON.stringify(nextState));
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log("componentDidUpdate: " + JSON.stringify(prevProps) + " " + JSON.stringify(prevState));
+    }
+
+    componentWillUnmount() {
+        console.log("componentWillUnmount");
+    }
+
     render() {
 
         return(
