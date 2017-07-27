791d7bb868934817811886376d79cbc7c81e9e8b
LeeJinHyuk <eerto@naver.com>
2017-07-27 09:54:14 +0900

    add mapStateTpProps test code

--- source/app/client/views/pages/main/main.js
+++ source/app/client/views/pages/main/main.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import {connect} from "react-redux";
 import {MainHeader} from "views/components";
 
 class Main extends Component {
@@ -13,4 +14,12 @@ class Main extends Component {
     }
 }
 
-export default Main;
+const mapStateToProps = (_state, _ownProps) => {
+  console.log(_state);
+  console.log(_ownProps);
+  return {
+    tab_index : _state.tab_index
+  };
+};
+
+export default connect(mapStateToProps)(Main);
